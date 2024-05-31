e37e8e2eeb17d3593758e1a53adbd02c4af58d79
Vikashlab <vikash7618@gmail.com>
2024-06-01 04:11:44 +0530

    Python program to find second largest number in a list

---
+++ QuestionNO17.py
@@ -0,0 +1,9 @@
+#Python program to find second largest number in a list
+
+lst=[3,5,9,8,2]
+
+sort_list=sorted(lst)
+
+second_no=sort_list[-2]
+
+print("Second largest number of the list",second_no)
