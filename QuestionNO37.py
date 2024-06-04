6ecb01d45aceb73d63900f003dd766f7dbe0672a
Vikashlab <vikash7618@gmail.com>
2024-06-05 04:41:01 +0530

    Merge Two Lists in Python

---
+++ QuestionNO37.py
@@ -0,0 +1,21 @@
+#Merge Two Lists in Python
+
+#using append method
+fisrtlist=[1,2,3,4,5]
+secondlist=[6,7,8,9]
+
+for i in secondlist:
+    fisrtlist.append(i)
+
+print(fisrtlist) 
+
+
+#using + operator
+
+list1=[11,22,33,44,55]
+list2=[66,77,88,99]
+
+list3=list1+list2
+
+print(list3)
+
