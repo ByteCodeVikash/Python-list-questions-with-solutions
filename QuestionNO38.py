7a0fa1d30b1753696deacdc42ad3a464d65b7335
Vikashlab <vikash7618@gmail.com>
2024-06-05 04:41:34 +0530

    Iterate over a list in Python

---
+++ QuestionNO38.py
@@ -0,0 +1,12 @@
+#Iterate over a list in Python
+
+my_list=[1,2,3,4,5,6,7,8,9]
+
+for i in my_list:
+    print(i)
+
+
+ #using list comprehension
+
+thislist=[11,22,33,44,55,66,77]
+[print(i) for i in thislist]   
