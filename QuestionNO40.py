ccb8fec52a89185bb99891be9a43d969cf3ff5b9
Vikashlab <vikash7618@gmail.com>
2024-06-05 04:43:24 +0530

    Python Program to Find Largest Number in a List

---
+++ QuestionNO40.py
@@ -0,0 +1,16 @@
+#Python Program to Find Largest Number in a List
+
+#using 'sort'
+thislist=[23,78,11,99,23,445,56]
+
+thislist.sort()
+
+print("Largest number in list",thislist[-1])
+
+#using 'max'
+
+my_list=[1,2,3,4,5,65,7,88]
+
+max_ele=max(my_list)
+
+print("Largest element in list",max_ele)
