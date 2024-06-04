64b824a3810239eb4053ea8bf95d7b37681c485b
Vikashlab <vikash7618@gmail.com>
2024-06-05 04:40:18 +0530

    Python - Ways to remove duplicates from list

---
+++ QuestionNO36.py
@@ -0,0 +1,9 @@
+#Python - Ways to remove duplicates from list
+
+my_list=[1,2,3,1,2,34,4,5,1,1]
+
+print("This is original list",my_list)
+
+my_list=list(set(my_list))
+
+print("the list after removing duplicate",my_list)
