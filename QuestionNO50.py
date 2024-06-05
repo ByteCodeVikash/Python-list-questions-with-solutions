284667be86230d83525ee6b86e470d9b379109d9
Vikashlab <vikash7618@gmail.com>
2024-06-06 04:51:37 +0530

    Python | Check if a given object is list or not

---
+++ QuestionNO50.py
@@ -0,0 +1,14 @@
+#Python | Check if a given object is list or not
+
+ini_list1=[1,2,3,4,5]
+ini_list2='12345'
+
+if isinstance(ini_list1,list):
+    print("your object is not a list")
+else:
+    print("your object is not a list")
+
+if isinstance(ini_list2,list):
+    print("Your object is a list")
+else:
+    print("your object is not a list")    
