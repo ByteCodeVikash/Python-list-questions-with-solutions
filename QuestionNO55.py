9dc6736c892111df033b12915a88bd1737e2a64d
Vikashlab <vikash7618@gmail.com>
2024-06-07 05:02:14 +0530

    Python | Perform append at beginning of list

---
+++ QuestionNO55.py
@@ -0,0 +1,9 @@
+#Python | Perform append at beginning of list
+
+test_list=[1,2,3,4,5]
+
+print("this is original list",str(test_list))
+
+test_list.insert(0,0)
+
+print("this is after insert element in the list",str(test_list))
