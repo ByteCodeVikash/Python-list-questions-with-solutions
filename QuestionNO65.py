c0dc2de810231ff4852e1f555d8c0a1f10dcfda7
Vikashlab <vikash7618@gmail.com>
2024-06-08 04:55:05 +0530

    Python | Get last N elements from given list

---
+++ QuestionNO65.py
@@ -0,0 +1,11 @@
+#Python | Get last N elements from given list
+
+test_list=[1,2,3,4,5,6,7,8,9]
+
+print("this is original list",str(test_list))
+    
+n=5
+
+test_list=test_list[-n:]
+
+print("this is last N element form given list",str(test_list))
