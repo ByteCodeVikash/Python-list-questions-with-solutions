fc55d8f86b0a1b8dc76a97aa705b7f9a7e0fa6d8
Vikashlab <vikash7618@gmail.com>
2024-06-09 05:03:34 +0530

    Python | Convert a list of characters into a string

---
+++ QuestionNO74.py
@@ -0,0 +1,12 @@
+#Python | Convert a list of characters into a string
+
+
+def convert(s):
+
+    new=""
+    for i in s:
+        new+=i
+    return new
+s=['V','I','K','A','S','H']
+print(convert(s))    
+
