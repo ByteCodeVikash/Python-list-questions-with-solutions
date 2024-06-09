a0c0b13b3c111546288f192d0f904d942a12bfdd
Vikashlab <vikash7618@gmail.com>
2024-06-10 04:41:53 +0530

    Python | How to sort a list of strings

---
+++ QuestionNO83.py
@@ -0,0 +1,18 @@
+#Python | How to sort a list of strings
+
+lst=['This','is','a','good','boy']
+
+lst.sort()
+
+print(lst)
+
+# using sorted function
+
+my_list=['I','am','a','coder']
+
+emp_list=[]
+
+for i in sorted(my_list):
+    emp_list.append(i)
+
+print(emp_list)    
