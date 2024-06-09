d0864a75aa2985d7276695ea82ec57bdf2cd63d7
Vikashlab <vikash7618@gmail.com>
2024-06-10 04:45:44 +0530

    Python | Generate random numbers within a given range and store in a list

---
+++ QuestionNO88.py
@@ -0,0 +1,9 @@
+#Python | Generate random numbers within a given range and store in a list
+
+import random
+
+print("Random integer between 0 and 9: ")
+
+for i in range(4,15):
+    y=random.randrange(9)
+    print(y)
